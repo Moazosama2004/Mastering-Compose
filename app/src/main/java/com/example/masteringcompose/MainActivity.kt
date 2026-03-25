9a3b0980eb8099410d13618a6820cd8fecea8205
Muaz osama <moazosama204@gmail.com>
2026-03-25 17:16:09 +0200

    Implement state management and update UI with Button and FirstCompose composable

--- app/src/main/java/com/example/masteringcompose/MainActivity.kt
+++ app/src/main/java/com/example/masteringcompose/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.masteringcompose.ui.theme.MasteringComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +25,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MasteringComposeTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            // STATES
+            var stringState by remember { mutableStateOf("Hello")}
+
+            // UI
+            Column(
+                modifier = Modifier.padding(92.dp)
+            ) {
+                Button(
+                    onClick = {
+                        stringState= "Hello, World"
+                    }
+                ) {
+                    Text("Click Me")
                 }
+                FirstCompose(stringState)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun FirstCompose(
+    state : String
+) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = state
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun GreetingPreview() {
-    MasteringComposeTheme {
-        Greeting("Android")
-    }
+private fun preview() {
+    FirstCompose("Test")
 }
