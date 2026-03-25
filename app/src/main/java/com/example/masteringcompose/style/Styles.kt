19de1012c6a076453ba76971d692889f214c1950
Muaz osama <moazosama204@gmail.com>
2026-03-25 20:43:32 +0200

    Basic Text Field - ADD FONT FAMILY

--- app/src/main/java/com/example/masteringcompose/style/Styles.kt
+++ app/src/main/java/com/example/masteringcompose/style/Styles.kt
@@ -2,12 +2,19 @@ package com.example.masteringcompose.style
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.masteringcompose.R
 
+val myFontFamily = FontFamily(
+    Font(R.font.fjallaone_regular , FontWeight.Normal)
+)
 val myStyle = TextStyle(
     color = Color.Red,
     fontStyle = FontStyle.Italic,
     fontSize = 32.sp ,
+    fontFamily = myFontFamily
 )
