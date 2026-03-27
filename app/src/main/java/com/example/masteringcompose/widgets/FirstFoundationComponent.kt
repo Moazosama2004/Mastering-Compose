c536ea5f941932ffed36c1a6d011b0ae03f84d34
Muaz osama <moazosama204@gmail.com>
2026-03-27 13:12:01 +0200

    Annotated String

--- app/src/main/java/com/example/masteringcompose/widgets/FirstFoundationComponent.kt
+++ app/src/main/java/com/example/masteringcompose/widgets/FirstFoundationComponent.kt
@@ -1,26 +1,31 @@
 package com.example.masteringcompose.widgets
 
 import androidx.compose.foundation.text.BasicText
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.masteringcompose.style.myStyle
 
 @Composable
 fun FirstFoundationComponent() {
-    var text by remember{
-        mutableStateOf("Hello, World")
-    }
-    BasicTextField(
-        value = text,
-        onValueChange = {
-            text = it
+    BasicText(
+        text = buildAnnotatedString {
+            withStyle(SpanStyle(color = Color.Blue)) {
+                append("@Muaz")
+            }
+
+            withStyle(SpanStyle(color = Color.Black)) {
+                append(" Good Morning..❤️")
+            }
         },
-        textStyle = myStyle
+        style = myStyle
     )
 }
 
